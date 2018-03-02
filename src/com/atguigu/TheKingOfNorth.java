c47123f30988f00208f531f4fa435e8f4a7e7762
Sevenhells <929547401@qq.com>
2018-03-02 20:27:43 +0800

    创建

---
+++ src/com/atguigu/TheKingOfNorth.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+public class TheKingOfNorth {
+	public static void main(String[] args) {
+		System.out.println("11111111111111");
+		System.out.println("22222222222222");
+	}
+}
